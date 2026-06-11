636518fb715604a1bfcae8a417c5a2b949151eef
zachariah-spencer <spencerzachariah@gmail.com>
2026-06-11 15:16:24 -0500

    Setup boilerplate structure

---
+++ app/game.rb
@@ -0,0 +1,24 @@
+class Game
+  attr_dr
+
+  def initialize args
+    
+  end
+
+  def tick
+    args.outputs.background_color = [40,40,40]
+    outputs.watch "Hello"
+  end
+
+  def input
+    
+  end
+
+  def calc
+    
+  end
+
+  def render
+    
+  end
+end
