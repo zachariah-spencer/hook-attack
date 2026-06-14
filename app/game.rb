cde21667202afd94ee5f42a530b8cdf0076b91c8
Zachariah <spencerzachariah@gmail.com>
2026-06-13 20:38:35 -0500

    Add player attack inputs and hitbox

--- app/game.rb
+++ app/game.rb
@@ -9,17 +9,31 @@ class Game
       y: 50,
       w: 32,
       h: 32,
-      r: 255,
-      g: 0,
-      b: 0,
+      r: 0,
+      g: 255,
+      b: 255,
       dx: 0,
       dy: 0,
-      direction: 1, 
+      face_direction: 1,
+      move_direction: 0, 
       acceleration: 0.6,
       deceleration: 0.35,
       max_speed: 5.0,
-      attacking: false,
-      attacked_tick: -100
+      attack_duration: 0.5.seconds,
+      attacked_tick: nil,
+    }
+    state.hook = {
+      x: (state.player.x / 2) - 4,
+      y: (state.player.y / 2) - 4,
+      w: 64,
+      h: 32,
+      r: 255,
+      g: 0,
+      b: 0,
+      a: 100,
+      direction: 1,
+      active: false,
+      hit_target: nil
     }
   end
 
@@ -30,20 +44,19 @@ class Game
   end
 
   def input
-    state.player_move_direction = 0
-    state.player_move_direction -= 1 if inputs.keyboard.left
-    state.player_move_direction += 1 if inputs.keyboard.right
-    state.player_attack = inputs.keyboard.key_down.space
+    state.player.move_direction = 0
+    state.player.move_direction -= 1 if inputs.keyboard.left
+    state.player.move_direction += 1 if inputs.keyboard.right
+    state.player_attack_input = inputs.keyboard.key_down.space
   end
 
   def calc
     calc_gravity
     calc_player_movement
+    calc_hook_movement
 
-    state.player.attacked_tick = Kernel.tick_count if state.player_attack
-    if state.player.attacked_tick.elapsed_time <= 0.5.seconds
-      outputs.watch "ATTACKING"
-    end
+    state.player.attacked_tick = Kernel.tick_count if state.player_attack_input && !player_attacking?
+    state.player.attacked_tick = nil if state.player.attacked_tick && !player_attacking?
 
     calc_collisions
   end
@@ -51,17 +64,16 @@ class Game
   def render
     outputs.background_color = [40,40,40]
     outputs.solids << state.player
-    outputs.watch "PLAYER X: #{state.player.x}"
-    outputs.watch "PLAYER DIRECTION: #{state.player.direction}"
-    outputs.watch "PLAYER DX: #{state.player.dx}"
+    outputs.solids << state.hook if player_attacking?
+    outputs.watch "PLAYER_ATTACKING: #{player_attacking?}"
   end
 
   def calc_gravity
-    state.player.y -= 5.0
+    state.player.y -= 2.5
   end
 
   def calc_player_movement
-    target_dx = state.player_move_direction * state.player.max_speed
+    target_dx = state.player.move_direction * state.player.max_speed
 
     if state.player.dx < target_dx
       state.player.dx = [state.player.dx + state.player.acceleration, target_dx].min
@@ -70,22 +82,42 @@ class Game
     end
     state.player.x += state.player.dx
 
-    state.player.direction = player_direction?
+    state.player.face_direction = player_direction?
   end
 
-  def player_direction?
-    if state.player_move_direction > 0
-      1
-    elsif state.player_move_direction < 0
-      -1
-    else
-      state.player.direction
+  def calc_hook_movement
+    state.hook.direction = state.player.face_direction
+
+    if state.hook.direction > 0
+      hook_side_x = state.player.x + (state.player.w)
+    elsif state.hook.direction < 0
+      hook_side_x = state.player.x - (state.player.w * 2)
     end
+
+    center_of_player_y = state.player.y + (state.player.h / 2 - state.hook.h / 2)
+    
+    state.hook.x = hook_side_x
+    state.hook.y = center_of_player_y
   end
 
   def calc_collisions
     state.player.x = 0 if state.player.x <= 0
     state.player.x = Grid.w - state.player.w if state.player.x >= Grid.w - state.player.w
     state.player.y = Grid.h if state.player.y <= (0 - state.player.h)
+  end
+
+  def player_direction?
+    if state.player.move_direction > 0
+      1
+    elsif state.player.move_direction < 0
+      -1
+    else
+      state.player.face_direction
+    end
+  end
+
+  def player_attacking?
+    return false unless state.player.attacked_tick
+    state.player.attacked_tick.elapsed_time <= state.player.attack_duration
   end
 end
